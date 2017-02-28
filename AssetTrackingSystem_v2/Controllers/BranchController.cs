fdd58a0854f16d7d64638ef71aa4bb9d70c39c8b
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-02-28 16:47:27 +0600

    - Add BranchController -Add Branch Create view

---
+++ AssetTrackingSystem_v2/Controllers/BranchController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AssetTrackingSystem_v2.Models;
+
+namespace AssetTrackingSystem_v2.Controllers
+{
+    public class BranchController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Branch branch)
+        {
+            return View();
+        }
+
+        public ActionResult PartialMenu()
+        {
+            /* Action is used to load the menu dynamically when traversing from page to page */
+            ViewBag.Menu = "Organization Info";
+            ViewBag.SubMenu = "Branch";
+
+            return PartialView("_PartialMenu");
+        }
+    }
+}
