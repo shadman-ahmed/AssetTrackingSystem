91ac63b814ad3651bafdab4c7d7e7436eb8c56c2
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-02-28 15:05:20 +0600

    add dynamic menu mech·an·ism

--- AssetTrackingSystem_v2/Controllers/OrganizationController.cs
+++ AssetTrackingSystem_v2/Controllers/OrganizationController.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AssetTrackingSystem_v2.Models;
 
 namespace AssetTrackingSystem_v2.Controllers
 {
-    public class HomeController : Controller
+    public class OrganizationController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Create()
         {
             PartialMenu();
 
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Organization organization)
+        {
             return View();
         }
 
         public ActionResult PartialMenu()
         {
+
             /* Action is used to load the menu dynamically when traversing from page to page */
-            ViewBag.Menu = "Index";
+            ViewBag.Menu = "Organization Info";
+            ViewBag.SubMenu = "Organization";
 
             return PartialView("_PartialMenu");
         }
---
+++ AssetTrackingSystem_v2/Models/Organization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Key]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Key]
+        [StringLength(50)]
+        public string ShortName { get; set; }
+
+    }
+}
