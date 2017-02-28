03cf4b5656a1eb9f7bbfcb6b93ce276e3f6e2e8b
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-02-28 16:31:22 +0600

    Update Branch model

--- AssetTrackingSystem_v2/Models/Branch.cs
+++ AssetTrackingSystem_v2/Models/Branch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,22 @@ namespace AssetTrackingSystem_v2.Models
 {
     public class Branch
     {
+
         public int Id { get; set; }
+
+        [Required]
+
         public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("^[A-Z]$")]
         public string ShortName { get; set; }
         public string Code { get; set; }    /* <OrganizationShortName>_<BranchShortName> */
         public Organization Organization { get; set; }
-        public int Organization_Id { get; set; }
+
+        [Display(Name = "Organization")] 
+        [Required]
+        public int OrganizationId { get; set; }
 
     }
 }
