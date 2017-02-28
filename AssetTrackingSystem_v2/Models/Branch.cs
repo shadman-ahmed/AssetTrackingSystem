42655786d3c6a1bd41cd0fc74057089735c4011a
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-02-28 15:35:43 +0600

    Add Model - Organization with  annotation - Branch Update View - Organization Setup Form

--- AssetTrackingSystem_v2/Models/Branch.cs
+++ AssetTrackingSystem_v2/Models/Branch.cs
@@ -1,27 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
 
 namespace AssetTrackingSystem_v2.Models
 {
-    public class Organization
+    public class Branch
     {
         public int Id { get; set; }
-
-        [Required]
-        [Key]
-        [StringLength(100)]
         public string Name { get; set; }
-
-        [Required]
-        [Key]
-        [StringLength(50)]
         public string ShortName { get; set; }
-
-        public List<Branch> Branches;
+        public string Code { get; set; }    /* <OrganizationShortName>_<BranchShortName> */
+        public Organization Organization { get; set; }
+        public int Organization_Id { get; set; }
 
     }
 }
