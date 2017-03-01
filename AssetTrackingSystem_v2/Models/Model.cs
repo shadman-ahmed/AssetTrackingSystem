f15109a193525ea9e5b2c64c27399d872476449a
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-03-01 16:31:19 +0600

    Add Model Class @ Models

---
+++ AssetTrackingSystem_v2/Models/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public int ManufacuturerId { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
