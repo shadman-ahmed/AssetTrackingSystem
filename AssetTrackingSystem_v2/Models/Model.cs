854c6ed9c1f69dc1e9d91c8c516edc4d9aee2ac2
tahmina <tahmina.islam56@gmail.com>
2017-03-01 22:50:27 +0600

    <add> property,validation in Model.cs

--- AssetTrackingSystem_v2/Models/Model.cs
+++ AssetTrackingSystem_v2/Models/Model.cs
@@ -12,9 +12,19 @@ namespace AssetTrackingSystem_v2.Models
 
         [Required]
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
         public Manufacturer Manufacturer { get; set; }
+
+        [Display(Name = "Manufacturer")]
+        [Required]
         public int ManufacuturerId { get; set; }
+
         public Category Category { get; set; }
+
+        [Display(Name = "Category")]
+        [Required]
         public int CategoryId { get; set; }
     }
 }
