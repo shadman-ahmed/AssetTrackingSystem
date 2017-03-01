3bf2e00629e3e429ab5c42c6db17890523ab0ec5
tahmina <tahmina.islam56@gmail.com>
2017-03-02 02:12:45 +0600

    <add> ManufacturerController in Controllers

---
+++ AssetTrackingSystem_v2/Controllers/ManufacturerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetTrackingSystem_v2.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+        // GET: Manufacturer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
