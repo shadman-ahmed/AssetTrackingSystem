7ab455bc31bf71fb626c86e9b44c427bab726d1c
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-03-01 16:03:11 +0600

    Add Location Model Class

---
+++ AssetTrackingSystem_v2/Models/Location.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [RegularExpression("^[A-Z0-9]$", ErrorMessage = "Only uppercase letters allowed")]
+        public string ShortName { get; set; }
+        public string Code { get; set; }    /* <BranchCode>_<Location ShortName> */
+    }
+}
