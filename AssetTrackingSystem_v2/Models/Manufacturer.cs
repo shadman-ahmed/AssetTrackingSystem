76fa6c661b9b9009ac586ef509650acd14dff996
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-03-01 16:24:57 +0600

    Add Manufacturer Model Class

---
+++ AssetTrackingSystem_v2/Models/Manufacturer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+    }
+}
