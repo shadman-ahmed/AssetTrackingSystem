107c76350b490779db6338527e76b513ef873db4
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-03-01 16:09:19 +0600

    Add GeneralCategory Model Class

---
+++ AssetTrackingSystem_v2/Models/GeneralCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class GeneralCategory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
+        [RegularExpression("^[A-Z]$", ErrorMessage = "Only uppercase letters allowed")]
+        public string ShortName { get; set; }
+    }
+}
