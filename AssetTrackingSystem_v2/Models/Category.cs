62538623b35a12739255056429b75aa13841b815
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2017-03-01 16:16:56 +0600

    Add Category Model Class

---
+++ AssetTrackingSystem_v2/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(3, MinimumLength = 3)]
+        [RegularExpression("^A-Z\\S$", ErrorMessage = "Only uppercase letters allowed with no empty space")]
+        public string ShortName { get; set; }
+        public string Code { get; set; }    /* <GeneralCategory Short Name>_<Category Short Name> */
+        public GeneralCategory GeneralCategory { get; set; }
+
+    }
+}
