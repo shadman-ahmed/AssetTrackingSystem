fd95732f9264ba978b21c5dbd172e0ccedcdc184
tahmina <tahmina.islam56@gmail.com>
2017-03-02 02:21:11 +0600

    <add> action in ModelController

--- AssetTrackingSystem_v2/Controllers/ModelController.cs
+++ AssetTrackingSystem_v2/Controllers/ModelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssetTrackingSystem_v2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,27 @@ namespace AssetTrackingSystem_v2.Controllers
 {
     public class ModelController : Controller
     {
-        // GET: Model
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Create()
         {
+            PartialMenuView();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Model model)
+        {
+            PartialMenuView();
+            return View();
+        }
+
+        public ActionResult PartialMenuView()
+        {
+            /* Action is used to load the menu dynamically when traversing from page to page */
+            ViewBag.Menu = "Asset Info";
+            ViewBag.SubMenu = "Model";
+
+            return PartialView("_PartialMenu");
         }
     }
 }
