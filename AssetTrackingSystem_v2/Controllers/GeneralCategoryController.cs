c14a77a746caa8f1f4cd28e41e204b3e7cf3da0e
tahmina <tahmina.islam56@gmail.com>
2017-03-07 23:59:34 +0600

    <update> GeneralCategoryController

--- AssetTrackingSystem_v2/Controllers/GeneralCategoryController.cs
+++ AssetTrackingSystem_v2/Controllers/GeneralCategoryController.cs
@@ -68,6 +68,88 @@ namespace AssetTrackingSystem_v2.Controllers
             return View(generalCategory);
         }
 
+        public ActionResult Delete(int id)
+        {
+            var generalCategory = _generalCategoryManager.GetById(id);
+            try
+            {
+                if (_generalCategoryManager.Remove(generalCategory))
+                {
+                    ViewBag.Msg = "Deleted successfully!";
+
+                    return View("Search");
+                }
+            }
+            catch (Exception exception)
+            {
+                //return HttpNotFound();
+                ViewBag.Msg = exception.GetType().ToString();
+
+            }
+
+            return View("Search");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var generalCategory = _generalCategoryManager.GetById((int)id);
+            return View(generalCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(GeneralCategory generalCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                //ModelState.Clear();
+
+                try
+                {
+                    if (_generalCategoryManager.Update(generalCategory))
+                    {
+                        ViewBag.Msg = "Updated successfully!";
+                        return View();
+                    }
+                }
+                catch (Exception exception)
+                {
+                    int NameExist = _generalCategoryManager.GetAll(c => c.Name == generalCategory.Name).Count();
+
+                    if (NameExist > 0)
+                    {
+                        ModelState.AddModelError("Name", "Name already exists in the system");
+                    }
+
+                    int ShortNameExist = _generalCategoryManager.GetAll(c => c.ShortName == generalCategory.ShortName).Count();
+
+                    if (ShortNameExist > 0)
+                    {
+                        ModelState.AddModelError("ShortName", "Short name already exists in the system");
+                    }
+
+                }
+            }
+            return View(generalCategory);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var generalCategory = _generalCategoryManager.GetById(id);
+
+            return View(generalCategory);
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
         public ActionResult PartialMenuView()
         {
             /* Action is used to load the menu dynamically when traversing from page to page */
@@ -75,6 +157,14 @@ namespace AssetTrackingSystem_v2.Controllers
             ViewBag.SubMenu = "General Category";
 
             return PartialView("_PartialMenu");
+        }
+
+        public JsonResult GetAllgeneralCategory()
+        {
+
+            var generalCategorys = _generalCategoryManager.GetAll(c => true).ToList();
+
+            return Json(generalCategorys, JsonRequestBehavior.AllowGet);
         }
     }
 }
