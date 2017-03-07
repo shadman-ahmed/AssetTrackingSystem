b9860d6921fa0be03a0227524ed1ed21767dead4
tahmina <tahmina.islam56@gmail.com>
2017-03-08 01:21:26 +0600

    <add> ManufacturerManager in BLL

---
+++ ATS.BLL/ManufacturerManager.cs
@@ -0,0 +1,44 @@
+﻿using ATS.DAL;
+using ATS.Model.Interfaces.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.BLL
+{
+    public class ManufacturerManager
+    {
+        private IManufacturerRepository _repository;
+
+        public ManufacturerManager()
+        {
+            _repository = new ManufacturerRepository();
+        }
+        public bool Add(Organization entity)
+        {
+            return _repository.Add(entity);
+        }
+
+        public bool Remove(Organization entity)
+        {
+            return _repository.Remove(entity);
+        }
+
+        public bool Update(Organization entity)
+        {
+            return _repository.Update(entity);
+        }
+
+        public Organization GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public ICollection<Organization> GetAll(Expression<Func<Organization, bool>> predicateExpression)
+        {
+            return _repository.GetAll(predicateExpression);
+        }
+    }
+}
