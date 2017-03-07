b61a701cd7b9bc9aa55fbbf19f5fd133f31d51d2
tahmina <tahmina.islam56@gmail.com>
2017-03-07 12:41:11 +0600

    <add> Employee model

---
+++ ATS.Model/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Code { get; set; }
+
+        public Organization Organization { get; set; }
+
+        [Display(Name = "Organization")]
+        [Required]
+        public int OrganizationId { get; set; }
+
+        public Branch Branch { get; set; }
+
+        [Display(Name = "Branch")]
+        [Required]
+        public int BranchId { get; set; }
+
+
+    }
+}
