5ff51a54cd9a87cd22fde99a9855b6c73bce5aef
tahmina <tahmina.islam56@gmail.com>
2017-03-09 01:20:30 +0600

    <add> Category_Code.js

---
+++ AssetTrackingSystem_v2/Scripts/AssetTracking/Category_Code.js
@@ -0,0 +1,41 @@
+﻿
+$(document).ready(function () {
+
+    /* Automatic Code generation from general category and shortname field */
+    $('#GeneralCategoryId, #ShortName').change(function () {
+
+        var generalCategoryId = $('#GeneralCategoryId').val();
+        var CategoryShortName = $('#ShortName').val();
+
+        var jsonData = { id: generalCategoryId };
+        $.ajax({
+            url: '/Category/GetgeneralCategoryById?id=' + generalCategoryId,
+            contentType: 'application/json',
+            data: JSON.stringify(jsonData),
+            success: function (generalCategory) {
+                console.log(generalCategory.Id);
+                var Code = generalCategory.ShortName + "_" + CategoryShortName;
+                $('#Code').val(Code);
+            }
+
+        });
+    });
+
+    $('#Code').FOCUS(function () {
+        var generalCategoryId = $('#GeneralCategoryId').val();
+        var CategoryShortName = $('#ShortName').val();
+
+        var jsonData = { id: generalCategoryId };
+        $.ajax({
+            url: '/Category/GetgeneralCategoryById?id=' + generalCategoryId,
+            contentType: 'application/json',
+            data: JSON.stringify(jsonData),
+            success: function (generalCategory) {
+                var Code = generalCategory.ShortName + "_" + CategoryShortName;
+                $('#Code').val(Code);
+            }
+
+        });
+    });
+
+});
