176954a039b4b6876242654af23197d7d5b0f69f
tahmina <tahmina.islam56@gmail.com>
2017-03-08 18:45:45 +0600

    <add> CategoryRepository in DAL

---
+++ ATS.DAL/CategoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using AssetTrackingSystem_v2.Models;
+using ATS.DAL.Base;
+using ATS.Model.Interfaces.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using AssetTrackingSystem_v2.DB;
+
+namespace ATS.DAL
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository, IDisposable
+    {
+        public AssetTrackingManagementDbContext Context
+        {
+            get { return _db as AssetTrackingManagementDbContext; }
+        }
+
+        public CategoryRepository():base(new AssetTrackingManagementDbContext())
+        {
+
+        }
+
+        public CategoryRepository(DbContext db) : base(db)
+        {
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
