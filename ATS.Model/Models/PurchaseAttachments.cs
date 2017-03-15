5283f22772cd2d09b3bb33da6d451e147e3a687c
tahmina <tahmina.islam56@gmail.com>
2017-03-16 01:30:48 +0600

    <add> PurchaseAttachments model

---
+++ ATS.Model/Models/PurchaseAttachments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Model.Models
+{
+    public class PurchaseAttachments
+    {
+        public int Id { get; set; }
+
+        public int PurchaseHeaderId { get; set; }
+
+        public byte[] FileData { get; set; }
+    }
+}
