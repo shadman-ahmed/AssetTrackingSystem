aa29d8861832b9ce2d862116e08675086eaf8130
tahmina <tahmina.islam56@gmail.com>
2017-03-16 00:59:13 +0600

    <add> PurchaseDetails model

---
+++ ATS.Model/Models/PurchaseDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Model.Models
+{
+    public class PurchaseDetails
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int PurchaseHeaderId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int WarrantyPeriod { get; set; }
+
+        public int WarrantyUnitId { get; set; }
+
+        public bool HasWarranty { get; set; }
+    }
+}
