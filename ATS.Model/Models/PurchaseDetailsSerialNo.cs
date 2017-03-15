0181100313b213efc71375d92ceebac34f805128
tahmina <tahmina.islam56@gmail.com>
2017-03-16 01:06:30 +0600

    <add> PurchaseDetailsSerialNo model

---
+++ ATS.Model/Models/PurchaseDetailsSerialNo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Model.Models
+{
+    public class PurchaseDetailsSerialNo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int SerialNo { get; set; }
+
+        [Required]
+        public int PurchaseDetailsId { get; set; }
+        
+    }
+}
