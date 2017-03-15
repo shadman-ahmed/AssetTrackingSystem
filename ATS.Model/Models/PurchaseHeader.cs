d9be273c9c663b0984e0d2a52be3ef5d62a24024
tahmina <tahmina.islam56@gmail.com>
2017-03-16 00:39:55 +0600

    <add> PurchaseHeader model

---
+++ ATS.Model/Models/PurchaseHeader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Model.Models
+{
+    public class PurchaseHeader
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required]
+        public int VendorId { get; set; }
+
+        public string PurchasedBy { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
