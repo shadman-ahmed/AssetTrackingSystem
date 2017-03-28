624200435fef02be8f90ee457ce647b918670aca
tahmina <tahmina.islam56@gmail.com>
2017-03-28 21:56:13 +0600

    <add> Asset Model

---
+++ ATS.Model/Models/Asset/Asset.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class Asset
+    {
+        public string Id { get; set; } //Asset ID format would be [GeneralCategoryCode][CategoryCode][6 digit numeric]
+
+        public int AssetNo { get; set; }
+
+        public Location Location { get; set; }
+
+        [Display(Name = "Location")]
+        public int LocationId { get; set; }
+
+        public Employee Employee { get; set; }
+
+        [Display(Name = "Employee")]
+        public int AssignTo { get; set; }   // EmployeeId
+
+        public int SerialNo { get; set; }
+
+    }
+}
