fb6311212a1b1b86331b7e9a8a8c35449bdcede4
tahmina <tahmina.islam56@gmail.com>
2017-03-28 21:57:23 +0600

    <add> AssetRegistration Model

---
+++ ATS.Model/Models/Asset/AssetRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class AssetRegistration
+    {
+        public int Id { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public Location Location { get; set; }
+
+        public int LocationId { get; set; }
+
+        public bool IsFromPurchase { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
