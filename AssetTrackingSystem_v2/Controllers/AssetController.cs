23147db129eef304f63aaa673cb37a8ba09fd3e4
tahmina <tahmina.islam56@gmail.com>
2017-03-28 22:24:46 +0600

    <add> AssetController

---
+++ AssetTrackingSystem_v2/Controllers/AssetController.cs
@@ -0,0 +1,35 @@
+﻿using AssetTrackingSystem_v2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetTrackingSystem_v2.Controllers
+{
+    public class AssetController : Controller
+    {
+        private ActionResult PartialMenuView(string subMenu)
+        {
+            /* Action is used to load the menu dynamically when traversing from page to page */
+            ViewBag.Menu = "Asset Info";
+            ViewBag.SubMenu = subMenu;
+
+            return PartialView("_PartialMenu");
+        }
+        public ActionResult Create()
+        {
+            ModelState.Clear();
+            PartialMenuView("AssetRegistration");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(AssetRegistrationVM assetRegistrationVM)
+        {
+            return View();
+        }
+
+        
+    }
+}
