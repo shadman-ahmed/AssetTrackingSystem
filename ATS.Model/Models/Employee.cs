c0514430bf3c47a264496f3a48a1f51c5e83b046
tahmina <tahmina.islam56@gmail.com>
2017-03-28 21:54:17 +0600

    <add> Employee Model

---
+++ ATS.Model/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackingSystem_v2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+    }
+}
