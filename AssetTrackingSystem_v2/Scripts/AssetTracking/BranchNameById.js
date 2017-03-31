ace458de724bf7a8ae38bde0a031709b478ae3cc
tahmina <tahmina.islam56@gmail.com>
2017-03-31 22:11:10 +0600

    <fix> Location Detail View

---
+++ AssetTrackingSystem_v2/Scripts/AssetTracking/BranchNameById.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+
+    var branchId = $('#BranchId').val();
+
+    var jsonData = { id: branchId };
+    $.ajax({
+        url: '/Loader/GetOrganizationById?id=' + branchId,
+        contentType: 'application/json',
+        data: JSON.stringify(jsonData),
+        success: function (branch) {
+            console.log(branch.Id);
+
+            var branchName = branch.Name;
+
+
+            $('#Branch').text(branchName);
+
+        }
+
+    });
+});
