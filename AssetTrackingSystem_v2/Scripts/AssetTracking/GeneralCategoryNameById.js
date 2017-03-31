862313d98f4360286b3bd9048bf393a796404ec9
tahmina <tahmina.islam56@gmail.com>
2017-03-31 21:59:20 +0600

    <fix> Category Detail View

---
+++ AssetTrackingSystem_v2/Scripts/AssetTracking/GeneralCategoryNameById.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    var generalCategoryId = $('#GeneralCategoryId').val();
+
+    var jsonData = { id: generalCategoryId };
+
+    $.ajax({
+        url: '/Loader/GetGeneralCategoryById?id=' + generalCategoryId,
+        contentType: 'application/json',
+        data: JSON.stringify(jsonData),
+        success: function (generalCategory) {
+            console.log(generalCategory.Id);
+
+            var generalCategoryName = generalCategory.Name;
+
+
+            $('#GeneralCategory').text(generalCategoryName);
+
+        }
+
+    });
+});
