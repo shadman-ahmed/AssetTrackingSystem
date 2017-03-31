b7a2c6fc0cada4918eec0f772445f458b56694a3
tahmina <tahmina.islam56@gmail.com>
2017-03-31 22:24:38 +0600

    <fix> Model Detail View

---
+++ AssetTrackingSystem_v2/Scripts/AssetTracking/CategoryNameById.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    var categoryId = $('#CategoryId').val();
+
+    var jsonData = { id: categoryId };
+
+    $.ajax({
+        url: '/Loader/GetCategoryById?id=' + categoryId,
+        contentType: 'application/json',
+        data: JSON.stringify(jsonData),
+        success: function (category) {
+            console.log(category.Id);
+
+            var categoryName = category.Name;
+
+
+            $('#category').text(categoryName);
+
+        }
+
+    });
+});
---
+++ AssetTrackingSystem_v2/Scripts/AssetTracking/ManufacturerNameById.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    var manufacturerId = $('#ManufacturerId').val();
+
+    var jsonData = { id: manufacturerId };
+
+    $.ajax({
+        url: '/Loader/GetManufacturerById?id=' + manufacturerId,
+        contentType: 'application/json',
+        data: JSON.stringify(jsonData),
+        success: function (manufacturer) {
+            console.log(manufacturer.Id);
+
+            var manufacturerName = manufacturer.Name;
+
+
+            $('#manufacturer').text(manufacturerName);
+
+        }
+
+    });
+});
