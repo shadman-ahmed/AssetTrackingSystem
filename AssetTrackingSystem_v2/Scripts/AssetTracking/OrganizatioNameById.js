25a99fea18e5f0a45a5046a0033d8af68864c71f
tahmina <tahmina.islam56@gmail.com>
2017-03-31 21:42:24 +0600

    <fix> branch Detail View

---
+++ AssetTrackingSystem_v2/Scripts/AssetTracking/OrganizatioNameById.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+
+    var organizationId = $('#OrganizationId').val();
+
+    var jsonData = { id: organizationId };
+    $.ajax({
+        url: '/Loader/GetOrganizationById?id=' + organizationId,
+        contentType: 'application/json',
+        data: JSON.stringify(jsonData),
+        success: function (organization) {
+            console.log(organization.Id);
+            
+            var organizationName = organization.Name;
+            
+
+            $('#Organization').text(organizationName);
+           
+        }
+
+    });
+});
