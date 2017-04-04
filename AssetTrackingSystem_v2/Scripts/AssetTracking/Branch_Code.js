669dc7255857556b7b40c8c51f1d7dd2b71a6fa3
tahmina <tahmina.islam56@gmail.com>
2017-04-04 23:29:00 +0600

    <update> Branch_Code.js

--- AssetTrackingSystem_v2/Scripts/AssetTracking/Branch_Code.js
+++ AssetTrackingSystem_v2/Scripts/AssetTracking/Branch_Code.js
@@ -1,40 +1,42 @@
 ﻿
+
 $(document).ready(function () {
 
-    /* Global variable */
-    var OrganizationShortName;
-    var organizationId = $('#OrganizationId').val();
-    //var branchShortName = $('#ShortName').val();
-
-    var jsonData = { id: organizationId };
-    $.ajax({
-        url: '/Loader/GetOrganizationById?id=' + organizationId,
-        contentType: 'application/json',
-        data: JSON.stringify(jsonData),
-        success: function (organization) {
-
-            OrganizationShortName = organization.ShortName;
-
-        }
-        });
-
     /* Automatic Code generation from organization and shortname field */
-    $('#OrganizationId').change(function () {
+    $('#OrganizationId, #ShortName').change(function () {
 
-        // + "_" + branchShortName;
-                $('#Code').val(OrganizationShortName);
-            
+        var organizationId = $('#OrganizationId').val();
+        var branchShortName = $('#ShortName').val();
+
+        var jsonData = { id: organizationId };
+        $.ajax({
+            url: '/Loader/GetOrganizationById?id=' + organizationId,
+            contentType: 'application/json',
+            data: JSON.stringify(jsonData),
+            success: function (organization) {
+                console.log(organization.Id);
+                var Code = organization.ShortName + "_" + branchShortName;
+                $('#Code').val(Code);
+            }
+
+        });
     });
-        
-   
 
-    /*$('#ShortName').keyup(function () {
-        var shortName = $('#ShortName').val();
-        $('#Code').val(OrganizationShortName + "_" + shortName);
-    });*/
-    $('#ShortName').bind('keyup change', function () {
-        var shortName = $('#ShortName').val();
-        $('#Code').val(OrganizationShortName + "_" + shortName);
+    $('#Code').focus(function () {
+        var organizationId = $('#OrganizationId').val();
+        var branchShortName = $('#ShortName').val();
+
+        var jsonData = { id: organizationId };
+        $.ajax({
+            url: '/Loader/GetOrganizationById?id=' + organizationId,
+            contentType: 'application/json',
+            data: JSON.stringify(jsonData),
+            success: function (organization) {
+                var Code = organization.ShortName + "_" + branchShortName;
+                $('#Code').val(Code);
+            }
+
+        });
     });
 
 });
